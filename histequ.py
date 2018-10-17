b8c5451eee9d4992f075d405c502f0be888f1bf4
Mahizzta <ksro17@student.aau.dk>
2018-10-17 21:45:25 +0200

    Added notes

--- histequ.py
+++ histequ.py
@@ -1,11 +1,10 @@
 import cv2
 import numpy as np
 
-img = cv2.imread('yarn.jpeg',0)
-equ = cv2.equalizeHist(img)
+img = cv2.imread('yarn.jpeg',0) #import image
+equ = cv2.equalizeHist(img) #equalize using opencv
 res = np.hstack((img,equ)) #stacking images side-by-side
-cv2.imwrite('res.png',res)
-cv2.imshow('res.png',res)
+cv2.imwrite('res.png',res) #save the image
+cv2.imshow('res.png',res) #show the image
 cv2.waitKey(0)
-
 cv2.destroyAllWindows()
